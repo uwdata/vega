2fc588fc8cfaaa778af100738098a9f604bde1f3
Ryan Russell <russellsprouts2@gmail.com>
2015-01-16 11:14:31 -0800

    Prefer for loops to forEach

--- src/transforms/copy.js
+++ src/transforms/copy.js
@@ -6,21 +6,25 @@ define(function(require, exports, module) {
     var as = [], fields = [], from;
 
     var node = new model.Node(function(input) {
+      var i, len, x, j, len2;
       util.debug(input, ["copying"]);
 
-      input.add.forEach(function(x) {
-        fields.forEach(function(f, i) {
-          //promote the field f of x[from] to field as[i] of x
-          x[as[i]] = (x[from] || {})[f];
-        });
-      });
 
-      input.mod.forEach(function(x) {
-        fields.forEach(function(f, i) {
-          //promote the field f of x[from] to field as[i] of x
-          x[as[i]] = (x[from] || {})[f];
-        });
-      });
+      for(i = 0, len = input.add.length; i < len; i++) {
+        x = input.add[i];
+        for(j = 0, len2 = fields.length; j < len2; j++) {
+          // promote the field fields[j] of x[from] to field as[i] of x;
+          x[as[j]] = (x[from] || {})[fields[j]];
+        }
+      }
+
+      for(i = 0, len = input.mod.length; i < len; i++) {
+        x = input.mod[i];
+        for(j = 0, len2 = fields.length; j < len2; j++) {
+          // promote the field fields[j] of x[from] to field as[i] of x;
+          x[as[j]] = (x[from] || {})[fields[j]];
+        }
+      }
 
       return input;
     });
