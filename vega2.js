2ca0bb8ae19f60bb29e2a761804a360896d07aab
Ryan Russell <russellsprouts2@gmail.com>
2015-01-21 10:06:45 -0800

    Allow dot notation in fields

--- vega2.js
+++ vega2.js
@@ -4215,26 +4215,30 @@ define('transforms/copy',['require','exports','module','../util/index'],function
 
   return function filter(model) {
 
-    var as = [], fields = [], from;
+    var as = [], // the names of the fields
+        fields = [], // the field accessors
+        from, // the from field accessor
+        from_base; // the first element of the from field (foo in foo.bar)
 
     var node = new model.Node(function(input) {
       var i, len, x, j, len2;
       util.debug(input, ["copying"]);
 
-
       for(i = 0, len = input.add.length; i < len; i++) {
         x = input.add[i];
         for(j = 0, len2 = fields.length; j < len2; j++) {
-          // promote the field fields[j] of x[from] to field as[i] of x;
-          x[as[j]] = (x[from] || {})[fields[j]];
+          // promote the field fields[j] of x[from] to field as[j] of x;
+          x[as[j]] = fields[j](from(x) || {})
         }
       }
 
-      for(i = 0, len = input.mod.length; i < len; i++) {
-        x = input.mod[i];
-        for(j = 0, len2 = fields.length; j < len2; j++) {
-          // promote the field fields[j] of x[from] to field as[i] of x;
-          x[as[j]] = (x[from] || {})[fields[j]];
+      if(input.fields[from_base]) {
+        for(i = 0, len = input.mod.length; i < len; i++) {
+          x = input.mod[i];
+          for(j = 0, len2 = fields.length; j < len2; j++) {
+            // promote the field fields[j] of x[from] to field as[i] of x;
+            x[as[j]] = fields[j](from(x) || {})
+          }
         }
       }
 
@@ -4242,12 +4246,16 @@ define('transforms/copy',['require','exports','module','../util/index'],function
     });
 
     node.from = function(field) {
-      from = field;
+      from = util.accessor(field);
+      from_base = util.field(field)[0];
       return node;
     };
 
     node.fields = function(fs) {
-      fields = fs;
+      fields = fs.map(util.accessor);
+      base_fields = fs.map(function(f) {
+        return util.field(f)[0];
+      });
       if(!as.length) as = fs;
       return node;
     };
