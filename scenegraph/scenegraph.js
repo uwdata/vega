947426aaced3190a41ff2eb973df8868acd55025
jhoffswell <jhoffs@cs.washington.edu>
2015-06-01 16:42:11 -0700

    Maintain Node Collapse
    Add functionality to maintain how nodes are collapsed or expanded
    between updates based on end-user interaction. Also enables first step
    of “smart default” to only show a simplified (collapsed) representation
    of the scenegraph.

--- scenegraph/scenegraph.js
+++ scenegraph/scenegraph.js
@@ -40,7 +40,7 @@
  *
  */
 
-var flatData, data, i, duration, root, tree, svg, diagonal;
+var oldData, newData, treeData, i, duration, root, tree, svg, diagonal;
 var init = false;
 
 /*************************************************************/
@@ -76,6 +76,7 @@ function debounce(fn, root, delay) {
 
 // Remove the old scenegraph and create a new one.
 function fullfillUpdate(root) {
+  console.log("Update scenegraph.");
   d3.select("#scenegraph").selectAll("*").remove();
   extractScenegraph(root);
 } // end fullfillUpdate
@@ -90,73 +91,126 @@ function updateScenegraph() {
 /******************** Extract Scenegraph *********************/
 /*************************************************************/
 
-function getID(group) {
-  var id = group.marktype + ".";
-  id += group.bounds.x1 + group.bounds.x2 + group.bounds.y1 + group.bounds.y2;
-  if(group.def.width && group.def.height) id += group.def.width + group.def.height;
-  if(group.def.from) id += group.def.from.data;
+// The scenegraph tree is created in a deterministic manner, so
+// this ID function uses the nested index of the nodes as the
+// unique ID of the node.
+function getID(node) {
+  var id = "";
+  // If the CURRENT node is a GROUP mark.
+  if(node.marktype != undefined) {
+    if(node.group) {
+      var parentID = getID(node.group);
+      var myIndex;
+      if(node.group.items.indexOf(node) != -1) {
+        myIndex = node.group.items.indexOf(node) + "i";
+      } else {
+        myIndex = node.group.axisItems.indexOf(node) + "a";
+      }
+      id = parentID + "." + myIndex;
+    } else {
+      // It is the root node.
+      id = "0";
+    }
+
+  // If the CURRENT node is an ITEM.
+  } else {
+    if(node.mark) {
+      var parentID = getID(node.mark);
+      var myIndex = node.mark.items.indexOf(node);
+      id = parentID + "." + myIndex;
+    }
+  }
   return id;
 } // end getID
 
+function getDataObj(currentNode) {
+  // Save relevant data for the scenegraph
+  var dataObj = {};
+  // TODO: figure out a better strategy for saving relevant information
+  if(currentNode.text != undefined) {
+    dataObj["text"] = currentNode.text;
+  }
+  if(currentNode.key != undefined) {
+    dataObj["key"] = currentNode.key;
+  }
+  if(currentNode.fill != undefined) {
+    dataObj["fill"] = currentNode.fill;
+  }
+  if(currentNode.fillOpacity != undefined) {
+    dataObj["fillOpacity"] = currentNode.fillOpacity;
+  }
+  if(currentNode.stroke != undefined) {
+    dataObj["stroke"] = currentNode.stroke;
+  }
+  if(currentNode.size != undefined) {
+    dataObj["size"] = currentNode.size;
+  }
+  if(currentNode.width != undefined) {
+    dataObj["width"] = currentNode.width;
+  }
+  if(currentNode.height != undefined) {
+    dataObj["height"] = currentNode.height;
+  }
+  if(currentNode.dx != undefined) {
+    dataObj["dx"] = currentNode.dx;
+  }
+  if(currentNode.x != undefined) {
+    dataObj["x"] = currentNode.x;
+  }
+  if(currentNode.x2 != undefined) {
+    dataObj["x2"] = currentNode.x2;
+  }
+  if(currentNode.dy != undefined) {
+    dataObj["dy"] = currentNode.dy;
+  }
+  if(currentNode.y != undefined) {
+    dataObj["y"] = currentNode.y;
+  }
+  if(currentNode.y2 != undefined) {
+    dataObj["y2"] = currentNode.y2;
+  }
+  if(currentNode.datum != undefined) {
+    dataObj["datum"] = currentNode.datum;
+  }
+  return dataObj;
+} // end getDataObj
+
 /*
  * There are two types of nodes in the scenegraph, Group marks and Items.
- * GROUP: <name>, <parent>, <type>
+ * GROUP: <name>, <parent>, <type>, <data>
  * ITEM:  <name>, <parent>, <data> 
  */
 function extractScenegraph(node) {
+  // Save the old data.
+  if(newData) {
+    oldData = newData;
+    newData = null;
+  }
   init = false;
+
+  // Save information about root node and prepare for tree traversal
   var nodes = [];
-
-  var ID = getID(node);
-  nodes.push({"name": ID, "type": node.marktype, "parent": "null"});
-
+  nodes.push({"name": getID(node), "type": node.marktype, "parent": "null"});
   var nodesToCheck = node.items.slice(0);
 
+  // Traverse scenegraph until all nodes have been reached
   while(nodesToCheck.length != 0) {
     var currentNode = nodesToCheck.pop();
     
+    // If the CURRENT node is a GROUP mark.
     if(currentNode.marktype != undefined) {
-      // If the CURRENT node is a GROUP mark.
-      var ID = getID(currentNode);
-      var parentID = currentNode.group._id;
-      var obj = {"name": ID, "type": currentNode.marktype, "parent": parentID};
+      var obj = {"name": getID(currentNode), 
+                 "parent": /*currentNode.group._id,*/ getID(currentNode.group),
+                 "type": currentNode.marktype,
+                 "data": currentNode.bounds};
       nodes.push(obj);
-
       nodesToCheck = nodesToCheck.concat(currentNode.items);
-
+    
+    // If the CURRENT node is an ITEM.
     } else {
-      // If the CURRENT node is an ITEM.
-      var parentID = getID(currentNode.mark)
-      var obj = {"name": currentNode._id, "parent": parentID};
-      
-      // If we are updating the scenegraph (i.e. we have data already),
-      // save the old data for comparison later.
-      if(flatData) {
-        var oldNode = flatData.filter(function(d) {
-          if(d.name == currentNode._id) {
-            return d;
-          }
-        })[0];
-        if(oldNode) {
-          obj["oldData"] = oldNode.data;
-          if(oldNode.myColor) obj["myColor"] = oldNode.myColor;
-        }
-      }
-
-      // Save relevant data for the scenegraph
-      var dataObj = {};
-      // TODO: figure out what information is important for an ITEM
-      if(currentNode.text != undefined) {
-        dataObj["text"] = currentNode.text;
-      }
-      if(currentNode.key != undefined) {
-        dataObj["key"] = currentNode.key;
-      }
-      if(currentNode.fill != undefined) {
-        dataObj["fill"] = currentNode.fill;
-      }
-
-      obj["data"] = dataObj;
+      var obj = {"name": /*currentNode._id, */ getID(currentNode),
+                 "parent": getID(currentNode.mark),
+                 "data": getDataObj(currentNode)};
       nodes.push(obj);
 
       if(currentNode.axisItems != undefined && currentNode.axisItems.length != 0) {
@@ -167,6 +221,7 @@ function extractScenegraph(node) {
       }
     }
   } // end while
+
   drawGraph(nodes);
 } // end extractScenegraph
 
@@ -175,18 +230,75 @@ function extractScenegraph(node) {
 /********************** Draw Scenegraph **********************/
 /*************************************************************/
 
+function dataChanged(oldDataObj, newDataObj) {
+  var somethingChanged = false;
+  //TODO: see if we can break out of this loop early.
+  Object.keys(newDataObj).forEach(function(key) {
+    if(newDataObj[key] != oldDataObj[key]) somethingChanged = true;
+  });
+  return somethingChanged;
+} // end dataChanged
+
+function processDiff() {
+  // For all the new data, determine if updated or added.
+  newData.forEach(function(node) {
+    var matchingNodes = oldData.filter(function(oldNode) {
+      if(oldNode.name == node.name) return oldNode;
+    });
+
+    // We would expect the oldData to only have ONE node with
+    // the same name as the input node (i.e. name should be unique)
+    if(matchingNodes.length > 1) {
+      console.log("ERROR: Multiple children");
+      console.log(matchingNodes)
+    }
+    if(matchingNodes.length == 0) {
+      node.status = "added";
+    } else if(matchingNodes[0].status && matchingNodes[0].status == "removed") {
+      console.log("Matched a removed node.");
+      // TODO: If this happens, we need to figure out what the proper
+      //       way to handle this is.
+    } else if(matchingNodes[0].data && dataChanged(matchingNodes[0].data, node.data)) {
+      if(matchingNodes[0].collapsed != undefined) {
+        node.collapsed = matchingNodes[0].collapsed;
+      }
+      node.status = "updated";
+    } else {
+      if(matchingNodes[0].collapsed != undefined) {
+        node.collapsed = matchingNodes[0].collapsed;
+      }
+      node.status = "none";
+    }
+
+  });
+
+  // For all the old data, determine if any nodes were removed.
+  oldData.forEach(function(oldNode) {
+    var newNode = newData.filter(function(d) {
+      if(d.name == oldNode.name) return d;
+    });
+    if(newNode.length == 0) {
+      console.log("Node removed!")
+      oldNode.status = "removed";
+      newData.push(oldNode);
+    }
+  });
+} // end processDiff
+
 // BASED ON: http://www.d3noob.org/2014/01/tree-diagrams-in-d3js_11.html
 //           http://bl.ocks.org/mbostock/4339083
 function drawGraph(nodes) {
+  // Preprocess the data.
+  newData = nodes.slice(0);
+  if(oldData) processDiff();
 
-  flatData = nodes.slice(0);
   // Structure the nodes appropriately.
   var dataMap = nodes.reduce(function(map, node) {
     map[node.name] = node;
     return map;
   }, {});
 
-  var treeData = [];
+  treeData = [];
     nodes.forEach(function(node) {
       var parent = dataMap[node.parent];
       if (parent) {
@@ -196,12 +308,11 @@ function drawGraph(nodes) {
       }
   });
 
-  data = treeData;
   var margin = {top: 20, right: 05, bottom: 20, left: 05};
   var width = d3.select("#vis")[0][0].offsetWidth - margin.right - margin.left;
   var height = 800 - margin.top - margin.bottom;
     
-  i = 0,
+  i = 0;
   duration = 750;
 
   tree = d3.layout.tree()
@@ -216,54 +327,17 @@ function drawGraph(nodes) {
     .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  root = data[0];
+  root = treeData[0];
   root.x0 = 0;
   root.y0 = height / 2;
 
-  //root.children.forEach(collapse);
-  //root.children.forEach(partialCollapse);
+  partialCollapse(root);
   update(root);
 
   d3.select(self.frameElement).style("height", "800px");
 } // end drawGraph
 
-// TODO: update functionality to look for repeated patterns in the
-//       structure of the scenegraph and collapse regularly repeated
-//       structures (e.g. scatterplot matrix)
-function partialCollapse(d) {
-  if(d.children) {
-    // Determine how many of the children are leaf nodes
-    var leaves = d.children.filter(function(child) {
-      if(!child.children || child.children.length == 0) return child;
-    });
-
-    // If most of the children are leaf nodes, collapse the node.
-    if(leaves.length >= AUTO_COLLAPSE_THREASHOLD) collapse(d);
-    // Otherwise, leave the node visible and recurse.
-    else d.children.forEach(partialCollapse);
-  }
-} // end partialCollapse
-
-function collapse(d) {
-  if(d.children) {
-    d._children = d.children;
-    d._children.forEach(collapse);
-    d.children = null;
-  }
-} // end collapse
-
-function expand(d) {
-  if(d._children) {
-    d.children = d._children;
-    d.children.forEach(expand);
-    d._children = null;
-  } else if (d.children) {
-    d.children.forEach(expand);
-  }
-} // end expand
-
 function update(source) {
-
   // Compute the new tree layout.
   var nodes = tree.nodes(root).reverse(),
       links = tree.links(nodes);
@@ -280,7 +354,9 @@ function update(source) {
       .attr("class", "node")
       .attr("transform", function(d) { return "translate(" + source.x0 + "," + source.y0 + ")"; })
       .on("click", function(d) {
+        // TODO: figure out how to disable this for double click.
         if(d.data) console.log(d.name + ":", d.data)
+        else console.log(d.name)
       })
       .on("dblclick", collapseNode);
 
@@ -297,7 +373,6 @@ function update(source) {
       .text(function(d) { 
         if(d.content != undefined) return d.content;
         if(d.type != undefined) return d.type;
-        else return d.name; 
       })
       .style("fill-opacity", 1e-6)
       .style("font-size", "8pt")
@@ -316,17 +391,10 @@ function update(source) {
   nodeUpdate.select("circle")
       .attr("r", 4.5)
       .style("stroke", function(d) {
-        if(d.data && d.oldData) {
-          var somethingChanged = false;
-          Object.keys(d.data).forEach(function(key) {
-            if(d.data[key] != d.oldData[key]) somethingChanged = true;
-          })
-          if(somethingChanged) {
-            d.myColor = "red";
-            return "red";
-          }
-        }
-        return d.myColor || "lightsteelblue";
+        if(d.status == "added") return "limegreen";
+        if(d.status == "removed") return "red";
+        if(d.status == "updated") return "orange";
+        return "lightsteelblue";
       })
       .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
 
@@ -386,29 +454,78 @@ function update(source) {
   });
 } // end update
 
-// Toggle children.
+// TODO: update functionality to look for repeated patterns in the
+//       structure of the scenegraph and collapse regularly repeated
+//       structures (e.g. scatterplot matrix)
+function partialCollapse(d) {
+  if(d.children) {
+    // Determine how many of the children are leaf nodes
+    var leaves = d.children.filter(function(child) {
+      if(!child.children || child.children.length == 0) return child;
+    });
+
+    // Check the previous collapsed status of the node.
+    if(d.collapsed != undefined && !d.collapsed) d.children.forEach(partialCollapse);
+    else if(d.collapsed) collapse(d);
+    // If most of the children are leaf nodes, collapse the node.
+    else if(leaves.length >= AUTO_COLLAPSE_THREASHOLD) collapse(d);
+    // Otherwise, leave the node visible and recurse.
+    else d.children.forEach(partialCollapse);
+  }
+} // end partialCollapse
+
+// If the node has children, hide the children and
+// recursively collpase the grandchildren.
+function collapse(d) {
+  if(d.children) {
+    setCollapsed(d, true);
+    d._children = d.children;
+    d._children.forEach(collapse);
+    d.children = null;
+  }
+} // end collapse
+
+function expand(d) {
+  if(d._children) {
+    setCollapsed(d, false);
+    d.children = d._children;
+    d.children.forEach(expand);
+    d._children = null;
+  } /*else if (d.children) {
+    d.children.forEach(expand);
+  }*/ //TODO: figure out why this is here.
+} // end expand
+
+// Toggle collapsed status of node.
 function collapseNode(d) {
-  if (d.children) {
+  if (d.children) { // Collapse the node.
+    setCollapsed(d, true);
     d._children = d.children;
     d.children = null;
-  } else {
+  } else { // Expand the node.
+    setCollapsed(d, false);
     d.children = d._children;
     d._children = null;
   }
   update(d);
 } // end collapseNode
 
+function setCollapsed(d, collapsed) {
+  newData.forEach(function(node) {
+    if(node.name == d.name) node.collapsed = collapsed;
+  });
+} // end setCollapsed
+
+// Hide or show all nodes from button click.
 function toggle() {
-  //TODO: figure out why it shows two nodes, but lets you collapse 
-  //      the first one thus breaking everything
   var button = d3.select("#btn_scene_show")[0][0];
   if(button.value == "Hide All") {
     button.value = "Show All";
-    root.children.forEach(collapse);
+    collapse(root);
     update(root)
   } else {
     button.value = "Hide All";
-    root.children.forEach(expand);
+    expand(root);
     update(root)
   }
 } // end toggle
